1711dd7eb40a00e6c3412d8f70b3ffdb3969ed2e
Joe Repasy <roguesyko@gmail.com>
2015-08-16 15:26:27 -0400

    Update broken.mk

--- broken.mk
+++ broken.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/lge/d852/full_d852.mk)
+$(call inherit-product, device/lge/d850/full_d850.mk)
 
 $(call inherit-product, vendor/broken/config/common_full_phone.mk)
 
@@ -9,6 +9,6 @@ PRODUCT_NAME := broken_d850
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
-    PRODUCT_NAME="g3_bell_ca" \
-    BUILD_FINGERPRINT="lge/g3_global_com/g3:5.0/LRX21R.A1421650137/1421650137:user/release-keys" \
-    PRIVATE_BUILD_DESC="g3_global_com-user 5.0 LRX21R.A1421650137 1421650137 release-keys"
+    PRODUCT_NAME="g3_att_us" \
+    BUILD_FINGERPRINT="lge/g3_att_us/g3:4.4.2/KVT49L.D85010d/D85010d.1403173065:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_att_us-user 4.4.2 KVT49L.D85010d D85010d.1403173065 release-keys"
