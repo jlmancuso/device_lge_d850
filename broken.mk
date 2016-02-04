cbca93e44a7bf0ff24342f40bb77eb3e29060b7d
roguesyko <roguesyko@gmail.com>
2016-02-04 13:06:14 -0500

    Add device maintainer

--- broken.mk
+++ broken.mk
@@ -12,3 +12,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="g3_att_us" \
     BUILD_FINGERPRINT="lge/g3_att_us/g3:5.0.1/LRX21Y/D85020f.1421382354:user/release-keys" \
     PRIVATE_BUILD_DESC="g3_att_us-user 5.0.1 LRX21Y D85020f.1421382354 release-keys"
+
+# Broken Device Maintainers
+    PRODUCT_BUILD_PROP_OVERRIDES += \
+ 	DEVICE_MAINTAINERS="Joe (roguesyko) Repasy" 
